30254f37cce300ff85f6caeb6b1631d574caa00d
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-05-23 07:55:13 +0300

    закончил дз

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -6,7 +6,7 @@ export const FriendList = ({ friends }) => {
   return <ul className={css.friend}>
   {friends.map(({ id, avatar, name, isOnline }) => (
     <li className={css.item} key={id}>
-      <span className={css.status}>{isOnline}</span>
+      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
